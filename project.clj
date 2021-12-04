a62e31693cadfc2f86750467ac87d3c7d156d5be
Ken Smith <ken.smith@2scientists.net>
2021-12-04 10:58:32 -0700

    Solutions to Day 01

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [advent-utils "0.1.1"]]
-  :repl-options {:init-ns advent2021.core})
+  :repl-options {:init-ns advent2021.day01})
