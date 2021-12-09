b7ac5f1b836c63db33d9afe4753641cc5937c8ce
Ken Smith <ken.smith@2scientists.net>
2021-12-09 06:19:56 -0700

    Day 09 solutions

---
+++ src/advent2021/day09.clj
@@ -0,0 +1,72 @@
+(ns advent2021.day09
+  (:require [advent-utils.core :as u]
+            [advent-utils.maze :as m]))
+
+(defn parse-line
+  [line]
+  (map (comp read-string str) line))
+
+(defn grid-of
+  [values]
+  (let [coords (for [y (range (count values))
+                     x (range (count (first values)))]
+                 [x y])]
+    (zipmap coords (flatten values))))
+
+(def day09-input
+  (grid-of (map parse-line (u/puzzle-input "day09-input.txt"))))
+
+(defn low-point?
+  [grid pos]
+  (let [val (grid pos)
+        coords (m/adj-coords pos)
+        n-vals (->> (map grid coords)
+                    (filter some?))]
+    (every? #(< val %) n-vals)))
+
+(defn risk-level
+  [val]
+  (inc val))
+
+(defn risk-level-sum
+  [input]
+  (let [low-points (filter (partial low-point? input) (keys input))
+        low-vals   (map input low-points)
+        risks      (map risk-level low-vals)]
+    (reduce + risks)))
+
+(defn day09-part1-soln
+  []
+  (risk-level-sum day09-input))
+
+(defn non-nine-neighbors
+  [grid pos]
+  (let [coords (m/adj-coords pos)]
+    (->> (filter (comp some? grid) coords)
+         (filter #(not= 9 (grid %))))))
+
+(defn basin
+  [grid low-point]
+  (loop [basin #{low-point} next (non-nine-neighbors grid low-point)]
+    (if (empty? next)
+      basin
+      (recur (into basin next)
+             (->> (mapcat (partial non-nine-neighbors grid) next)
+                  (filter (complement basin)))))))
+
+(defn basin-sizes
+  [grid]
+  (let [low-points (filter (partial low-point? grid) (keys grid))
+        basins     (map (partial basin grid) low-points)]
+    (map count basins)))
+
+(defn three-largest-basins-product
+  [grid]
+  (->> (basin-sizes grid)
+       (sort >)
+       (take 3)
+       (reduce *)))
+
+(defn day09-part2-soln
+  []
+  (three-largest-basins-product day09-input))
