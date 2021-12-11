7119e75d710c908e724804a0232789390090eea0
Ken Smith <ken.smith@2scientists.net>
2021-12-11 12:44:25 -0700

    Day 11 solutions parts 1 and 2

--- src/advent2021/day11.clj
+++ src/advent2021/day11.clj
@@ -1,4 +1,74 @@
 (ns advent2021.day11
   (:require [advent-utils.core :as u]))
 
-(def day11-input (u/puzzle-input "day11-input.txt"))
+(defn parse-line
+  [line]
+  (map (comp read-string str) line))
+
+;; TODO: add to common utils
+(defn grid-of
+  [values]
+  (let [coords (for [y (range (count values))
+                     x (range (count (first values)))]
+                 [x y])]
+    (zipmap coords (flatten values))))
+
+(def day11-input
+  (->> (u/puzzle-input "day11-input.txt")
+       (map parse-line)
+       grid-of))
+
+(defn to-flash
+  [grid flashed]
+  (->> (filter #(> (val %) 9) grid)
+       keys
+       (filter (complement flashed))))
+
+(defn neighbors
+  [[x y]]
+  (->> (for [nx (range (dec x) (+ x 2))
+             ny (range (dec y) (+ y 2))]
+         [nx ny])
+       (filter #(not= [x y] %))))
+
+(defn flash
+  [grid flashers flashed]
+  (let [valid?     (set (keys grid))
+        adjacents (->> (mapcat neighbors flashers)
+                       (filter valid?))
+        next-grid (reduce #(update %1 %2 inc) grid adjacents)
+        next-flashed (into flashed flashers)]
+    [next-grid
+     (to-flash next-grid next-flashed)
+     next-flashed]))
+
+(defn step
+  [[total-flashed grid]]
+  (loop [next-grid (u/fmap inc grid)
+         flashers (to-flash next-grid #{})
+         flashed #{}]
+    (if (empty? flashers)
+      [(+ total-flashed (count flashed))
+       (reduce #(assoc %1 %2 0) next-grid flashed)]
+      (let [[a b c] (flash next-grid flashers flashed)]
+        (recur a b c)))))
+
+(defn flashes-after-100-steps
+  [grid]
+  (first (nth (iterate step [0 grid]) 100)))
+
+(defn day11-part1-soln
+  []
+  (flashes-after-100-steps day11-input))
+
+(defn steps-until-sync
+  [grid]
+  (->> (iterate step [0 grid])
+       (map-indexed
+        (fn [idx [_ new-grid]]
+          [idx (every? zero? (vals new-grid))]))
+       (filter second)
+       ffirst))
+(defn day11-part2-soln
+  []
+  (steps-until-sync day11-input))
--- test/advent2021/day11_test.clj
+++ test/advent2021/day11_test.clj
@@ -2,10 +2,33 @@
   (:require [clojure.test :refer [deftest testing is]]
             [advent2021.day11 :as t]))
 
-;; (deftest day11-part1-soln
-;;   (testing "Reproduces the answer for day11, part1"
-;;     (is (= 394647 (t/day11-part1-soln)))))
+(def day11-sample
+  (->>
+   ["5483143223"
+    "2745854711"
+    "5264556173"
+    "6141336146"
+    "6357385478"
+    "4167524645"
+    "2176841721"
+    "6882881134"
+    "4846848554"
+    "5283751526"]
+   (map t/parse-line)
+   t/grid-of))
 
-;; (deftest day11-part2-soln
-;;   (testing "Reproduces the answer for day11, part2"
-;;     (is (= 2380061249 (t/day11-part2-soln)))))
+(deftest flashes-after-100-steps
+  (testing "Counts total number of flashes after 100 steps in sample data"
+    (is (= 1656 (t/flashes-after-100-steps day11-sample)))))
+
+(deftest steps-until-sync
+  (testing "Counts number of steps before all flashes are synchronized"
+    (is (= 195 (t/steps-until-sync day11-sample)))))
+
+(deftest day11-part1-soln
+  (testing "Reproduces the answer for day11, part1"
+    (is (= 1634 (t/day11-part1-soln)))))
+
+(deftest day11-part2-soln
+  (testing "Reproduces the answer for day11, part2"
+    (is (= 210 (t/day11-part2-soln)))))
