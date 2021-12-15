9f003ba775f8850fcab52eac70bce1a5c4f94e35
Ken Smith <ken.smith@2scientists.net>
2021-12-15 05:55:04 -0700

    Day 15 part1 soln

--- src/advent2021/day15.clj
+++ src/advent2021/day15.clj
@@ -1,4 +1,70 @@
 (ns advent2021.day15
-  (:require [advent-utils.core :as u]))
+  (:require [advent-utils.core :as u]
+            [advent-utils.maze :as m]
+            [advent-utils.graph :as g :refer [Graph ->MapGraph]]))
 
-;; (def day15-input (u/puzzle-input "day15-input.txt"))
+(defn parse-line
+  [line]
+  (map (comp read-string str) line))
+
+(defn parse
+  [lines]
+  (map parse-line lines))
+
+;; TODO: add to common utils
+;; Same function used in day09, day11, and now day15
+(defn grid-of
+  [values]
+  (let [coords (for [y (range (count values))
+                     x (range (count (first values)))]
+                 [x y])]
+    (zipmap coords (flatten values))))
+
+(def day15-sample
+  (grid-of
+   (parse
+    ["1163751742"
+     "1381373672"
+     "2136511328"
+     "3694931569"
+     "7463417111"
+     "1319128137"
+     "1359912421"
+     "3125421639"
+     "1293138521"
+     "2311944581"])))
+
+(def day15-input (grid-of (parse (u/puzzle-input "day15-input.txt"))))
+
+;; TODO: possible better implementation for m/neighbors
+(defn neighbor-graph
+  [grid pos]
+  (let [coords (->> (m/adj-coords pos)
+                    (filter (comp some? grid)))
+        values (map grid coords)]
+    (zipmap coords values)))
+
+(defn graph-of
+  [grid]
+  (->MapGraph
+   (zipmap (keys grid)
+           (map (partial neighbor-graph grid) (keys grid)))))
+
+(defn max-pos
+  [grid]
+  (let [maxx (apply max (map first (keys grid)))
+        maxy (apply max (map second (keys grid)))]
+    [maxx maxy]))
+
+(defn find-path-vals
+  [grid]
+  (let [path (g/dijkstra (graph-of grid) [0 0] (max-pos grid))]
+    (map grid path)))
+
+(defn path-risk
+  [input]
+  (reduce + (rest (find-path-vals input))))
+
+(defn day15-part1-soln
+  []
+  (path-risk day15-input))
--- test/advent2021/day15_test.clj
+++ test/advent2021/day15_test.clj
@@ -2,9 +2,33 @@
   (:require [clojure.test :refer [deftest testing is]]
             [advent2021.day15 :as t]))
 
+(def day15-sample
+  (t/grid-of
+   (t/parse
+    ["1163751742"
+     "1381373672"
+     "2136511328"
+     "3694931569"
+     "7463417111"
+     "1319128137"
+     "1359912421"
+     "3125421639"
+     "1293138521"
+     "2311944581"])))
+
+(deftest find-path-vals
+  (testing "Finds the risk values along the safest path in sample data"
+    (is (= '(1 1 2 1 3 6 5 1 1 1 5 1 1 3 2 3 2 1 1)
+           (t/find-path-vals day15-sample)))))
+
+(deftest find-path-risk
+  (testing "Finds the total risk along the safest path in sample data"
+    (is (= 40 (t/path-risk day15-sample)))))
+
+;; Correct, but too slow to run as a regular unit test
 ;; (deftest day15-part1-soln
 ;;   (testing "Reproduces the answer for day15, part1"
-;;     (is (= 394647 (t/day15-part1-soln)))))
+;;     (is (= 508 (t/day15-part1-soln)))))
 
 ;; (deftest day15-part2-soln
 ;;   (testing "Reproduces the answer for day15, part2"
