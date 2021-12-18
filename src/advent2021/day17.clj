5ac7c593bd08d89c67f45a105e1e24b8a8fba95c
Ken Smith <ken.smith@2scientists.net>
2021-12-17 21:45:53 -0700

    Day 17 WIP

--- src/advent2021/day17.clj
+++ src/advent2021/day17.clj
@@ -1,4 +1,59 @@
-(ns advent2021.day17
-  (:require [advent-utils.core :as u]))
+(ns advent2021.day17)
 
-;; (def day17-input (u/puzzle-input "day17-input.txt"))
+
+;; target area: x=20..30, y=-10..-5
+(def day17-sample
+  {:xmin 20
+   :xmax 30
+   :ymin -10
+   :ymax -5})
+
+;; target area: x=281..311, y=-74..-54
+(def day17-input
+  {:xmin 281
+   :xmax 311
+   :ymin -74
+   :ymax -54})
+
+(defn target?
+  [{:keys [xmin xmax ymin ymax]} [x y]]
+  (and (<= xmin x xmax) (<= ymin y ymax)))
+
+(defn step
+  [[[x y] [vx vy]]]
+  [[(+ x vx) (+ y vy)]
+   [(if (pos? vx) (dec vx) 0) (- vy 1)]])
+
+(defn evolve
+  [[vx vy] {:keys [ymin]}]
+  (take-while
+   #(>= (second (first %)) ymin)
+   (iterate step [[0 0] [vx vy]])))
+
+(defn hits-target?
+  [target init-velocity]
+  (boolean (some (partial target? target)
+                 (map first (evolve init-velocity target)))))
+
+(defn min-init-vx
+  [{:keys [xmin]}]
+  (int (/ (+ 1 (Math/sqrt (+ 1 (* 4 2 xmin)))) 2)))
+
+(defn highest-point
+  [vel target]
+  (apply max (map (comp second first) (evolve vel target))))
+
+(defn highest-trajectory
+  [target]
+  (let [vx    26 ;(min-init-vx target)
+        cands (map (partial vector vx) (range 4 100))]
+    (last (take-while (partial hits-target? target) cands))))
+
+(hits-target? day17-input [25 4])
+(min-init-vx day17-input)
+(evolve [24 30] day17-input)
+(highest-point [24 22] day17-input)
+(highest-trajectory day17-input)
+
+
+
--- test/advent2021/day17_test.clj
+++ test/advent2021/day17_test.clj
@@ -2,6 +2,21 @@
   (:require [clojure.test :refer [deftest testing is]]
             [advent2021.day17 :as t]))
 
+;; target area: x=20..30, y=-10..-5
+(def day17-sample
+  {:xmin 20
+   :xmax 30
+   :ymin -10
+   :ymax -5})
+
+(deftest hits-target?
+  (testing "Correctly identifies whether probe ever hits target"
+    (is (= true  (t/hits-target? day17-sample [7 2])))
+    (is (= true  (t/hits-target? day17-sample [6 3])))
+    (is (= true  (t/hits-target? day17-sample [9 0])))
+    (is (= true  (t/hits-target? day17-sample [6 9])))
+    (is (= false (t/hits-target? day17-sample [17 -4])))))
+
 ;; (deftest day17-part1-soln
 ;;   (testing "Reproduces the answer for day17, part1"
 ;;     (is (= 394647 (t/day17-part1-soln)))))
