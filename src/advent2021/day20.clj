9b30da5573d9a2625081fc6deab85e8a0dd30d09
Ken Smith <ken.smith@2scientists.net>
2021-12-20 15:22:43 -0700

    Day 20 part 1 solution

--- src/advent2021/day20.clj
+++ src/advent2021/day20.clj
@@ -1,4 +1,80 @@
 (ns advent2021.day20
-  (:require [advent-utils.core :as u]))
+  (:require [clojure.string :as str]
+            [advent-utils.ascii :as a]
+            [advent-utils.core :as u]))
 
-;; (def day20-input (u/puzzle-input "day20-input.txt"))
+;; TODO: Candidate for common utility
+(defn split-at-blankline
+  [input]
+  (let [chunks (-> (str/join "\n" input)
+                   (str/split #"\n\n"))]
+    (map #(str/split % #"\n") chunks)))
+
+(def char-map {\. 0 \# 1})
+
+(defn parse
+  [input]
+  (let [[part1 part2] (split-at-blankline input)]
+    {:algorithm (mapv char-map (first part1))
+     :image (a/ascii->map char-map part2)}))
+
+(def day20-sample
+  (parse
+   ["..#.#..#####.#.#.#.###.##.....###.##.#..###.####..#####..#....#..#..##..###..######.###...####..#..#####..##..#.#####...##.#.#..#.##..#.#......#.###.######.###.####...#.##.##..#..#..#####.....#.#....###..#.##......#.....#..#..#..##..#...##.######.####.####.#.#...#.......#..#.#.#...####.##.#......#..#...##.#.##..#...##.#.##..###.#......#.#.......#.#.#.####.###.##...#.....####.#..#..#.##.#....##..#.####....##...##..#...#......#.#.......#.......##..####..#...#.#.#...##..#.#..###..#####........#..####......#..#"
+    ""
+    "#..#."
+    "#...."
+    "##..#"
+    "..#.."
+    "..###"]))
+
+(def day20-input (parse (u/puzzle-input "day20-input.txt")))
+
+;; TODO variant of adj-coords. Consider consolidating
+(defn three-by-cell
+  [[x y]]
+  (for [ny (range (dec y) (+ y 2))
+        nx (range (dec x) (+ x 2))]
+    [nx ny]))
+
+;; TODO make this a utility fn
+(defn binstr->int
+  [s]
+  (Integer/parseInt s 2))
+
+(defn next-pixel
+  [{:keys [algorithm image]} pos]
+  (->> (three-by-cell pos)
+       (map #(get-in image [:grid %] 0))
+       (apply str)
+       binstr->int
+       (get algorithm)))
+
+(defn enhance
+  [{:keys [image] :as input}]
+  (let [{:keys [width height]} image
+        coords (for [y (range -1 (+ height 1))
+                     x (range -1 (+ width 1))]
+                 [x y])
+        vals   (map (partial next-pixel input) coords)
+        adj-coords (map (fn [[a b]] [(inc a) (inc b)]) coords)]
+    (assoc input :image
+           {:width (+ 2 width)
+            :height (+ 2 height)
+            :grid (zipmap adj-coords vals)})))
+
+(defn enhance-n-times
+  [input n]
+  (nth (iterate enhance input) n))
+
+(defn illuminated
+  [{:keys [image]}]
+  (->> image
+       :grid
+       vals
+       (filter pos?)
+       count))
+
+(defn day20-part1-soln
+  []
+  (illuminated (enhance-n-times day20-input 2)))
--- test/advent2021/day20_test.clj
+++ test/advent2021/day20_test.clj
@@ -2,9 +2,23 @@
   (:require [clojure.test :refer [deftest testing is]]
             [advent2021.day20 :as t]))
 
-;; (deftest day20-part1-soln
-;;   (testing "Reproduces the answer for day20, part1"
-;;     (is (= 394647 (t/day20-part1-soln)))))
+(def day20-sample
+  (t/parse
+   ["..#.#..#####.#.#.#.###.##.....###.##.#..###.####..#####..#....#..#..##..###..######.###...####..#..#####..##..#.#####...##.#.#..#.##..#.#......#.###.######.###.####...#.##.##..#..#..#####.....#.#....###..#.##......#.....#..#..#..##..#...##.######.####.####.#.#...#.......#..#.#.#...####.##.#......#..#...##.#.##..#...##.#.##..###.#......#.#.......#.#.#.####.###.##...#.....####.#..#..#.##.#....##..#.####....##...##..#...#......#.#.......#.......##..####..#...#.#.#...##..#.#..###..#####........#..####......#..#"
+    ""
+    "#..#."
+    "#...."
+    "##..#"
+    "..#.."
+    "..###"]))
+
+(deftest illuminated-after-2-enhance
+  (testing "Computes the number of pixels lit up after 2 enhancements"
+    (is (= 35 (t/illuminated (t/enhance-n-times day20-sample 2))))))
+
+(deftest day20-part1-soln
+  (testing "Reproduces the answer for day20, part1"
+    (is (= 5186 (t/day20-part1-soln)))))
 
 ;; (deftest day20-part2-soln
 ;;   (testing "Reproduces the answer for day20, part2"
