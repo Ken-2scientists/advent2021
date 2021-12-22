2aa9a1da4401ec2aa679072b60633bdc43081cd2
Ken Smith <ken.smith@2scientists.net>
2021-12-22 07:58:40 -0600

    Day 21 Part 1 soln

--- src/advent2021/day21.clj
+++ src/advent2021/day21.clj
@@ -1,4 +1,50 @@
 (ns advent2021.day21
   (:require [advent-utils.core :as u]))
 
+;; Sample data
+;; Player 1 starting position: 4
+;; Player 2 starting position: 8
+(def day21-sample [3 7])
+
 ;; (def day21-input (u/puzzle-input "day21-input.txt"))
+(def day21-input [8 9])
+
+(defn deterministic-die
+  [roll]
+  (take 3 (u/rotate (* 3 roll) (range 1 101))))
+
+(defn turn
+  [{:keys [roll player spaces score die]}]
+  (let [move     (reduce + (die roll))
+        newspace (mod (+ move (get spaces player)) 10)
+        newscore (+ 1 newspace (get score player))]
+    {:roll   (inc roll)
+     :player (if (zero? player) 1 0)
+     :spaces (assoc spaces player newspace)
+     :score  (assoc score player newscore)
+     :die    die}))
+
+(defn play
+  [input]
+  (let [start {:roll 0
+               :player 0
+               :spaces input
+               :score [0 0]
+               :die deterministic-die}]
+    (iterate turn start)))
+
+(defn no-winner?
+  [{:keys [score]}]
+  (< (apply max score) 1000))
+
+(defn play-until-win
+  [input]
+  (first (drop-while no-winner? (play input))))
+
+(defn loser-score-times-die-rolls
+  [{:keys [score roll]}]
+  (* 3 roll (apply min score)))
+
+(defn day21-part1-soln
+  []
+  (loser-score-times-die-rolls (play-until-win day21-input)))
--- test/advent2021/day21_test.clj
+++ test/advent2021/day21_test.clj
@@ -2,9 +2,24 @@
   (:require [clojure.test :refer [deftest testing is]]
             [advent2021.day21 :as t]))
 
-;; (deftest day21-part1-soln
-;;   (testing "Reproduces the answer for day21, part1"
-;;     (is (= 394647 (t/day21-part1-soln)))))
+;; Sample data
+;; Player 1 starting position: 4
+;; Player 2 starting position: 8
+(def day21-sample [3 7])
+
+(deftest roll-until-win
+  (testing "Determines when game ends for sample positions"
+    (is (= {:roll 331 :player 1 :spaces [9 2] :score [1000 745]}
+           (select-keys (t/play-until-win day21-sample)
+                        [:roll :player :spaces :score])))))
+
+(deftest loser-score-times-die-rolls
+  (testing "Computes the product of the loser score and number of die rolls"
+    (is (= 739785 (t/loser-score-times-die-rolls (t/play-until-win day21-sample))))))
+
+(deftest day21-part1-soln
+  (testing "Reproduces the answer for day21, part1"
+    (is (= 707784 (t/day21-part1-soln)))))
 
 ;; (deftest day21-part2-soln
 ;;   (testing "Reproduces the answer for day21, part2"
