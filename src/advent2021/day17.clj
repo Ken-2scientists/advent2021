0e5cc5e853d1bdb207e415d401474029de732b47
Ken Smith <ken.smith@2scientists.net>
2021-12-25 06:49:51 -0700

    Day 17 part 2 soln

--- src/advent2021/day17.clj
+++ src/advent2021/day17.clj
@@ -1,13 +1,5 @@
 (ns advent2021.day17)
 
-
-;; target area: x=20..30, y=-10..-5
-(def day17-sample
-  {:xmin 20
-   :xmax 30
-   :ymin -10
-   :ymax -5})
-
 ;; target area: x=281..311, y=-74..-54
 (def day17-input
   {:xmin 281
@@ -25,35 +17,60 @@
    [(if (pos? vx) (dec vx) 0) (- vy 1)]])
 
 (defn evolve
-  [[vx vy] {:keys [ymin]}]
+  [{:keys [ymin]} [vx vy]]
   (take-while
    #(>= (second (first %)) ymin)
    (iterate step [[0 0] [vx vy]])))
 
 (defn hits-target?
-  [target init-velocity]
+  [target trajectory]
   (boolean (some (partial target? target)
-                 (map first (evolve init-velocity target)))))
+                 (map first trajectory))))
+
+(defn will-hit-target?
+  [target vel]
+  (hits-target? target (evolve target vel)))
 
 (defn min-init-vx
   [{:keys [xmin]}]
   (int (/ (+ 1 (Math/sqrt (+ 1 (* 4 2 xmin)))) 2)))
 
-(defn highest-point
-  [vel target]
-  (apply max (map (comp second first) (evolve vel target))))
-
 (defn highest-trajectory
+  [target vymax]
+  (let [vx     (min-init-vx target)
+        speeds (for [vy (range vymax)]
+                 [vx vy])
+        trajectories (map (partial evolve target) speeds)
+        candidates (filter (partial hits-target? target) trajectories)
+        heights (map (fn [c] [(second (first c))
+                              (apply max (map (comp second first) c))]) candidates)]
+    (apply max-key second heights)))
+
+(defn highest-point
   [target]
-  (let [vx    26 ;(min-init-vx target)
-        cands (map (partial vector vx) (range 4 100))]
-    (last (take-while (partial hits-target? target) cands))))
+  (second (highest-trajectory target 100)))
 
-(hits-target? day17-input [25 4])
-(min-init-vx day17-input)
-(evolve [24 30] day17-input)
-(highest-point [24 22] day17-input)
-(highest-trajectory day17-input)
+(defn day17-part1-soln
+  []
+  (highest-point day17-input))
 
+;; TODO this is a brute force approach, and could
+;; be made considerably smarter by calculating the time
+;; range that the projectile is in the target's x range 
+;; for a given vx value, and then computing viable vys 
+;; based on the time range
+(defn viable-init-vels
+  [target vymax]
+  (let [vxmin (min-init-vx target)
+        vxmax (inc (:xmax target))
+        vymin (:ymin target)
+        speeds (for [vx (range vxmin vxmax)
+                     vy (range vymin vymax)]
+                 [vx vy])
+        trajectories (map (partial evolve target) speeds)
+        candidates (filter (partial hits-target? target) trajectories)]
+    (count candidates)))
 
-
+(defn day17-part2-soln
+  []
+  (viable-init-vels day17-input 100))
--- test/advent2021/day17_test.clj
+++ test/advent2021/day17_test.clj
@@ -11,16 +11,20 @@
 
 (deftest hits-target?
   (testing "Correctly identifies whether probe ever hits target"
-    (is (= true  (t/hits-target? day17-sample [7 2])))
-    (is (= true  (t/hits-target? day17-sample [6 3])))
-    (is (= true  (t/hits-target? day17-sample [9 0])))
-    (is (= true  (t/hits-target? day17-sample [6 9])))
-    (is (= false (t/hits-target? day17-sample [17 -4])))))
+    (is (= true  (t/will-hit-target? day17-sample [7 2])))
+    (is (= true  (t/will-hit-target? day17-sample [6 3])))
+    (is (= true  (t/will-hit-target? day17-sample [9 0])))
+    (is (= true  (t/will-hit-target? day17-sample [6 9])))
+    (is (= false (t/will-hit-target? day17-sample [17 -4])))))
 
-;; (deftest day17-part1-soln
-;;   (testing "Reproduces the answer for day17, part1"
-;;     (is (= 394647 (t/day17-part1-soln)))))
+(deftest viable-init-vels
+  (testing "Identifies the viable initial velocities that hit the sample target"
+    (is (= 112 (t/viable-init-vels day17-sample 10)))))
 
-;; (deftest day17-part2-soln
-;;   (testing "Reproduces the answer for day17, part2"
-;;     (is (= 2380061249 (t/day17-part2-soln)))))
+(deftest day17-part1-soln
+  (testing "Reproduces the answer for day17, part1"
+    (is (= 2701 (t/day17-part1-soln)))))
+
+(deftest day17-part2-soln
+  (testing "Reproduces the answer for day17, part2"
+    (is (= 1070 (t/day17-part2-soln)))))
