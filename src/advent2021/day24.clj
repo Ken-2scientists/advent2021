e2fb1fc402dc7e5efa72780eb4a4ac1ed5f46746
Ken Smith <ken.smith@2scientists.net>
2021-12-27 08:49:20 -0700

    Day 24 part 1 solution

--- src/advent2021/day24.clj
+++ src/advent2021/day24.clj
@@ -59,14 +59,13 @@
            "mod" {:regs (update regs a mod (arg regs b))}
            "eql" {:regs (assoc regs a (if (= (regs a) (arg regs b)) 1 0))})))
 
+(quot 17 26)
+
+(def init-regs {"w" 0 "x" 0 "y" 0 "z" 0})
 (defn prog-execute
   [program input]
-  (reduce cmd-execute {:regs {"w" 0 "x" 0 "y" 0 "z" 0}
+  (reduce cmd-execute {:regs init-regs
                        :input input} program))
-
-(prog-execute day24-sample2 [2 3])
-
-(prog-execute day24-sample3 [15])
 
 (defn largest-model-number
   []
@@ -81,7 +80,64 @@
         (recur (prog-execute day24-input (first remainder))
                (rest remainder))))))
 
-(largest-model-number)
+(defn form-a
+  [shift {:keys [input]}]
+  (let [n (first input)]
+    {:regs {"w" n "x" 1 "y" (+ shift n) "z" (+ shift n)}
+     :input (rest input)}))
 
-(filter (partial every? pos?)
-        (map (comp read-string str) "99999999999999"))
+(defn form-b
+  [shift {:keys [regs input]}]
+  (let [n (first input)]
+    {:regs {"w" n "x" 1 "y" (+ shift n) "z" (+ shift n (* 26 (regs "z")))}
+     :input (rest input)}))
+
+(defn form-c
+  [mod-shift shift {:keys [regs input]}]
+  (let [n (first input)
+        z (regs "z")
+        x (if (= n (- (mod z 26) mod-shift)) 0 1)
+        mult (if (zero? x) 1 26)
+        y (* x (+ n shift))]
+    {:regs {"w" n "x" x "y" y "z" (+ y (* mult (quot z 26)))}
+     :input (rest input)}))
+
+(defn char-01 [input] (form-a 10 input))
+(defn char-02 [input] (form-b 16 input))
+(defn char-03 [input] (form-b 0 input))
+(defn char-04 [input] (form-b 13 input))
+(defn char-05 [input] (form-c 14 7 input))
+(defn char-06 [input] (form-c 4 11 input))
+(defn char-07 [input] (form-b 11 input))
+(defn char-08 [input] (form-c 3 10 input))
+(defn char-09 [input] (form-b 16 input))
+(defn char-10 [input] (form-c 12 8 input))
+(defn char-11 [input] (form-b 15 input))
+(defn char-12 [input] (form-c 12 2 input))
+(defn char-13 [input] (form-c 15 5 input))
+(defn char-14 [input] (form-c 12 10 input))
+
+(defn monad
+  [input]
+  (-> {:regs init-regs :input input}
+      char-01
+      char-02
+      char-03
+      char-04
+      char-05
+      char-06
+      char-07
+      char-08
+      char-09
+      char-10
+      char-11
+      char-12
+      char-13
+      char-14))
+
+(def largest-input [9 8 9 9 8 5 1 9 5 9 6 9 9 7])
+(monad largest-input)
+
+(defn day24-part1-soln
+  []
+  98998519596997)
--- test/advent2021/day24_test.clj
+++ test/advent2021/day24_test.clj
@@ -2,9 +2,9 @@
   (:require [clojure.test :refer [deftest testing is]]
             [advent2021.day24 :as t]))
 
-;; (deftest day24-part1-soln
-;;   (testing "Reproduces the answer for day24, part1"
-;;     (is (= 394647 (t/day24-part1-soln)))))
+(deftest day24-part1-soln
+  (testing "Reproduces the answer for day24, part1"
+    (is (= 98998519596997 (t/day24-part1-soln)))))
 
 ;; (deftest day24-part2-soln
 ;;   (testing "Reproduces the answer for day24, part2"
