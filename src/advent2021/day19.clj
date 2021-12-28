bbdc601d4ac6538db20e568cdf045f9cb680d0c1
Ken Smith <ken.smith@2scientists.net>
2021-12-28 11:13:18 -0700

    Day 19 WIP. Can find orientation

--- src/advent2021/day19.clj
+++ src/advent2021/day19.clj
@@ -1,6 +1,7 @@
 (ns advent2021.day19
   (:require [clojure.set :as set]
             [clojure.string :as str]
+            [clojure.core.matrix :as matrix]
             [advent-utils.core :as u]))
 
 (defn parse-sensor
@@ -26,8 +27,64 @@
     "-5,0"
     "-2,1"]))
 
+;; TODO Candidates for a common math utility
+(def rotation-x-90
+  [[1 0  0]
+   [0 0 -1]
+   [0 1  0]])
+
+(def rotation-y-90
+  [[0 0 1]
+   [0 1 0]
+   [-1 0 0]])
+
+(def rotation-z-90
+  [[0 -1 0]
+   [1 0  0]
+   [0 0 1]])
+
+(def all-orientation-permutations
+  [[[1  0 0] [0  1  0] [0  0  1]]; identity
+   [[1  0 0] [0 -1  0] [0  0 -1]]; rot-x rot-x
+   [[1  0 0] [0  0  1] [0 -1  0]]; rot-x rot-x rot-x
+   [[1  0 0] [0  0 -1] [0  1  0]]; rot-x
+
+   [[-1 0 0] [0  1  0] [0  0 -1]]; rot-x rot-x rot-z rot-z
+   [[-1 0 0] [0 -1  0] [0  0  1]]; rot-z rot-z
+   [[-1 0 0] [0  0  1] [0  1  0]]; rot-x rot-x rot-x rot-z rot-z
+   [[-1 0 0] [0  0 -1] [0 -1  0]]; rot-x rot-z rot-z
+
+   [[0  1 0] [1  0  0] [0  0 -1]]; rot-x rot-x rot-z rot-z rot-z
+   [[0  1 0] [-1 0  0] [0  0  1]]; rot-z rot-z rot-z
+   [[0  1 0] [0  0  1] [1  0  0]]; rot-x rot-x rot-x rot-z rot-z rot-z rot-z
+   [[0  1 0] [0  0 -1] [-1 0  0]]; rot-x rot-z rot-z rot-z
+
+   [[0 -1 0] [1  0  0] [0  0  1]]; rot-z
+   [[0 -1 0] [-1 0  0] [0  0 -1]]; rot-x rot-x rot-z
+   [[0 -1 0] [0  0  1] [-1 0  0]]; rot-x rot-x rot-x rot-z
+   [[0 -1 0] [0  0 -1] [1  0  0]]; rot-x rot-z
+
+   [[0  0 1] [1  0  0] [0  1 0]]; rot-x rot-y
+   [[0  0 1] [-1 0  0] [0 -1 0]]; rot-x rot-x rot-x rot-y
+   [[0  0 1] [0  1  0] [-1 0 0]]; rot-y
+   [[0  0 1] [0 -1  0] [1  0 0]]; rot-x rot-x rot-y
+
+   [[0 0 -1] [1  0  0] [0 -1 0]]; rot-x rot-x rot-x rot-y rot-y rot-y
+   [[0 0 -1] [-1 0  0] [0  1 0]]; rot-x rot-y rot-y rot-y
+   [[0 0 -1] [0  1  0] [1  0 0]]; rot-y rot-y rot-y
+   [[0 0 -1] [0 -1  0] [-1 0 0]]; rot-x rot-x rot-y rot-y rot-y 
+   ])
+
 (def day19-sample1 (parse (u/puzzle-input "day19-sample1.txt")))
 (def day19-input  (parse (u/puzzle-input "day19-input.txt")))
+
+(comment
+  (matrix/mmul rotation-x-90 rotation-x-90 rotation-x-90 rotation-y-90 rotation-y-90 rotation-y-90))
+
+(defn matrix-mult
+  "Matrix multiply with v represented as a row (not column) vector"
+  [matrix v]
+  (mapv #(reduce + (map * % v)) matrix))
 
 
 ;; Overall thoughts on the approach.
@@ -44,7 +101,9 @@
 ;;  * Repeat for all the other sensors until we've uniquely mapped everything into 
 ;;    a common coordinate system
 
-
+(defn abs
+  [x]
+  (Math/abs x))
 
 (defn dist
   "Manhattan distance"
@@ -61,23 +120,49 @@
     (->> (mapv #(u/rotate % beacons) indices)
          (map dist-to-rest))))
 
-(relative-distances (first day19-sample))
+(defn relative-vectors
+  [beacons idx]
+  (let [shifted (u/rotate idx beacons)]
+    (map #(mapv - % (first shifted)) (rest shifted))))
 
-(def seta (into #{} (flatten (relative-distances (first day19-sample1)))))
-seta
+(defn overlap
+  [a b]
+  (set/intersection (set a) (set b)))
 
-(def setb (into #{} (flatten (relative-distances (second day19-sample1)))))
-setb
+(defn rel-dist-overlaps
+  [rel-dist-a rel-dist-b]
+  (apply concat
+         (map-indexed
+          (fn [a-pos a]
+            (map-indexed (fn [b-pos b]
+                           [[a-pos b-pos] (overlap a b)]) rel-dist-b)) rel-dist-a)))
 
-(set/intersection seta setb)
+(defn overlapping-relative-vectors
+  [overlap-set beacons offset]
+  (set (filter #(overlap-set (reduce + (map abs %))) (relative-vectors beacons offset))))
+
+(defn at-least-twelve-overlap
+  [rel-dist-a rel-dist-b]
+  (->> (rel-dist-overlaps rel-dist-a rel-dist-b)
+       (filter #(>= ((comp count second) %) 11))
+       first))
+
+(defn find-orientation
+  [beacons-a beacons-b [[idx-a idx-b] overlap-set]]
+  (let [rel-vecs-a (overlapping-relative-vectors overlap-set beacons-a idx-a)
+        rel-vecs-b (overlapping-relative-vectors overlap-set beacons-b idx-b)
+        permutations (map-indexed (fn [idx m]
+                                    [idx (set (map (fn [v] (matrix-mult m v)) rel-vecs-b))]) all-orientation-permutations)]
+    (get all-orientation-permutations (ffirst (filter #(= rel-vecs-a (second %)) permutations)))))
+
+(defn reorient
+  [orientation beacons]
+  (map (partial matrix-mult orientation) beacons))
+
+(def rel-a (relative-distances (first day19-sample1)))
+(def rel-b (relative-distances (second day19-sample1)))
+(def overlap-set #{188 141 1233 2346 2112 1489 2357 1361 1331 1372 1380})
+(def orientation (find-orientation (first day19-sample1) (second day19-sample1) (at-least-twelve-overlap rel-a rel-b)))
+(reorient orientation (second day19-sample1))
 
 
-(relative-distances day19-sample)
-
-(def foo (first day19-sample))
-
-(map (partial dist (first foo)) (rest foo))
-
-
-(count (first day19-sample))
-(relative-distances (first day19-sample))
