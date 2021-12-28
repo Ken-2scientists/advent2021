ff4fdb811d12cc5e786d24efe8023c1d54ede21b
Ken Smith <ken.smith@2scientists.net>
2021-12-28 14:58:18 -0700

    Got day 19 finally!

--- src/advent2021/day19.clj
+++ src/advent2021/day19.clj
@@ -1,31 +1,21 @@
 (ns advent2021.day19
   (:require [clojure.set :as set]
             [clojure.string :as str]
-            [clojure.core.matrix :as matrix]
             [advent-utils.core :as u]))
 
-(defn parse-sensor
-  [sensor]
-  (mapv #(read-string (str "[" % "]"))
-        (rest (str/split sensor #"\n"))))
-
-(defn parse
-  [input]
-  (let [sensors (-> (str/join "\n" input)
-                    (str/split #"\n\n"))]
-    (mapv parse-sensor sensors)))
-
-(def day19-sample
-  (parse
-   ["--- scanner 0 ---"
-    "0,2"
-    "4,1"
-    "3,3"
-    ""
-    "--- scanner 1 ---"
-    "-1,-1"
-    "-5,0"
-    "-2,1"]))
+;; Overall thoughts on the approach.
+;;  * The relative position vector between any two beacons that can both be seen
+;;    by a sensor will be the same, modulo the orientation of the sensors.
+;;  * Relative distance isn't affected by orientation of the sensors
+;;  * Figure out which beacons *might* be both seen by different sensors by:
+;;    * Compute the relative distances between all beacons seen by sensor A
+;;    * For each other sensor, find out which distances are in common
+;;  * For distances in common, compute all rotations of the relative position vectors
+;;    * One set of the relative position vectors must match (Now we know the orientation)
+;;    * For a given pair from each sensor with the same relative position vector, we can
+;;      then compute the office.
+;;  * Repeat for all the other sensors until we've uniquely mapped everything into 
+;;    a common coordinate system
 
 ;; TODO Candidates for a common math utility
 (def rotation-x-90
@@ -75,94 +65,159 @@
    [[0 0 -1] [0 -1  0] [-1 0 0]]; rot-x rot-x rot-y rot-y rot-y 
    ])
 
-(def day19-sample1 (parse (u/puzzle-input "day19-sample1.txt")))
-(def day19-input  (parse (u/puzzle-input "day19-input.txt")))
+(defn parse-sensor
+  [sensor]
+  (mapv #(read-string (str "[" % "]"))
+        (rest (str/split sensor #"\n"))))
 
-(comment
-  (matrix/mmul rotation-x-90 rotation-x-90 rotation-x-90 rotation-y-90 rotation-y-90 rotation-y-90))
+(defn parse
+  [input]
+  (let [sensors (-> (str/join "\n" input)
+                    (str/split #"\n\n"))]
+    (zipmap (range (count sensors))
+            (mapv #(assoc {} :beacons (parse-sensor %)) sensors))))
+
+(defn abs
+  [x]
+  (Math/abs x))
+
+(defn relative-vectors
+  [beacons idx]
+  (let [shifted (u/rotate idx beacons)]
+    (mapv #(mapv - % (first shifted)) (rest shifted))))
+
+(defn vector-signature
+  [vec]
+  (set (map abs vec)))
+
+(defn relative-vector-signatures
+  [rel-vecs]
+  (mapv #(mapv vector-signature %) rel-vecs))
+
+(defn add-relative-vectors
+  [{:keys [beacons] :as sensor}]
+  (let [indices (range (count beacons))
+        rel-vecs (mapv (partial relative-vectors beacons) indices)
+        sigs     (relative-vector-signatures rel-vecs)]
+    (assoc sensor
+           :rel-vecs rel-vecs
+           :rel-vec-sigs sigs)))
+
+(def day19-sample
+  (u/fmap add-relative-vectors
+          (parse
+           ["--- scanner 0 ---"
+            "0,2"
+            "4,1"
+            "3,3"
+            ""
+            "--- scanner 1 ---"
+            "-1,-1"
+            "-5,0"
+            "-2,1"])))
+
+(def day19-input  (u/fmap add-relative-vectors (parse (u/puzzle-input "day19-input.txt"))))
 
 (defn matrix-mult
   "Matrix multiply with v represented as a row (not column) vector"
   [matrix v]
   (mapv #(reduce + (map * % v)) matrix))
 
-
-;; Overall thoughts on the approach.
-;;  * The relative position vector between any two beacons that can both be seen
-;;    by a sensor will be the same, modulo the orientation of the sensors.
-;;  * Relative distance isn't affected by orientation of the sensors
-;;  * Figure out which beacons *might* be both seen by different sensors by:
-;;    * Compute the relative distances between all beacons seen by sensor A
-;;    * For each other sensor, find out which distances are in common
-;;  * For distances in common, compute all rotations of the relative position vectors
-;;    * One set of the relative position vectors must match (Now we know the orientation)
-;;    * For a given pair from each sensor with the same relative position vector, we can
-;;      then compute the office.
-;;  * Repeat for all the other sensors until we've uniquely mapped everything into 
-;;    a common coordinate system
-
-(defn abs
-  [x]
-  (Math/abs x))
-
 (defn dist
   "Manhattan distance"
   [a b]
   (reduce + (map (comp #(Math/abs %) -) a b)))
 
-(defn dist-to-rest
-  [items]
-  (map (partial dist (first items)) (rest items)))
-
-(defn relative-distances
-  [beacons]
-  (let [indices (range (count beacons))]
-    (->> (mapv #(u/rotate % beacons) indices)
-         (map dist-to-rest))))
-
-(defn relative-vectors
-  [beacons idx]
-  (let [shifted (u/rotate idx beacons)]
-    (map #(mapv - % (first shifted)) (rest shifted))))
-
 (defn overlap
   [a b]
   (set/intersection (set a) (set b)))
 
-(defn rel-dist-overlaps
-  [rel-dist-a rel-dist-b]
+(defn rel-vec-overlaps
+  [sensor-a sensor-b]
   (apply concat
          (map-indexed
           (fn [a-pos a]
             (map-indexed (fn [b-pos b]
-                           [[a-pos b-pos] (overlap a b)]) rel-dist-b)) rel-dist-a)))
-
-(defn overlapping-relative-vectors
-  [overlap-set beacons offset]
-  (set (filter #(overlap-set (reduce + (map abs %))) (relative-vectors beacons offset))))
+                           [[a-pos b-pos] (overlap a b)]) (:rel-vec-sigs sensor-b))) (:rel-vec-sigs sensor-a))))
 
 (defn at-least-twelve-overlap
-  [rel-dist-a rel-dist-b]
-  (->> (rel-dist-overlaps rel-dist-a rel-dist-b)
+  [sensor-a sensor-b]
+  (->> (rel-vec-overlaps sensor-a sensor-b)
        (filter #(>= ((comp count second) %) 11))
        first))
 
+(defn overlapping-relative-vectors
+  [overlap-set sensor offset]
+  (set (filter #(overlap-set (vector-signature %)) (get (:rel-vecs sensor) offset))))
+
 (defn find-orientation
-  [beacons-a beacons-b [[idx-a idx-b] overlap-set]]
-  (let [rel-vecs-a (overlapping-relative-vectors overlap-set beacons-a idx-a)
-        rel-vecs-b (overlapping-relative-vectors overlap-set beacons-b idx-b)
+  [sensor-a sensor-b [[idx-a idx-b] overlap-set]]
+  (let [rel-vecs-a (overlapping-relative-vectors overlap-set sensor-a idx-a)
+        rel-vecs-b (overlapping-relative-vectors overlap-set sensor-b idx-b)
         permutations (map-indexed (fn [idx m]
-                                    [idx (set (map (fn [v] (matrix-mult m v)) rel-vecs-b))]) all-orientation-permutations)]
-    (get all-orientation-permutations (ffirst (filter #(= rel-vecs-a (second %)) permutations)))))
+                                    [idx (set (map (fn [v] (matrix-mult m v)) rel-vecs-b))]) all-orientation-permutations)
+        match      (ffirst (filter #(= rel-vecs-a (second %)) permutations))]
+    (when (nil? match)
+      (println rel-vecs-a rel-vecs-b))
+    (get all-orientation-permutations match)))
 
 (defn reorient
-  [orientation beacons]
-  (map (partial matrix-mult orientation) beacons))
+  [orientation sensor]
+  (update sensor :beacons (partial mapv (partial matrix-mult orientation))))
 
-(def rel-a (relative-distances (first day19-sample1)))
-(def rel-b (relative-distances (second day19-sample1)))
-(def overlap-set #{188 141 1233 2346 2112 1489 2357 1361 1331 1372 1380})
-(def orientation (find-orientation (first day19-sample1) (second day19-sample1) (at-least-twelve-overlap rel-a rel-b)))
-(reorient orientation (second day19-sample1))
+(defn find-offset
+  [sensor-a sensor-b [idx-a idx-b]]
+  (mapv - (get-in sensor-a [:beacons idx-a]) (get-in sensor-b [:beacons idx-b])))
 
+(defn shift
+  [offset sensor]
+  (update sensor :beacons (partial mapv #(mapv + offset %))))
 
+(defn orient-sensor-to-known
+  [known sensor]
+  (let [overlap (at-least-twelve-overlap known sensor)]
+    (if (not overlap)
+      nil
+      (let [orientation (find-orientation known sensor overlap)
+            oriented    (reorient orientation sensor)
+            offset      (find-offset known oriented (first overlap))
+            shifted     (shift offset oriented)]
+        (assoc (add-relative-vectors shifted)
+               :orientation orientation
+               :offset offset)))))
+
+(defn orient-against-all-known
+  [known unknown]
+  (first (filter some? (map #(orient-sensor-to-known % unknown) (vals known)))))
+
+(defn orient-all-sensors
+  [sensors]
+  (loop [oriented (select-keys sensors [0])
+         to-orient (u/without-keys sensors [0])]
+    (if (empty? to-orient)
+      oriented
+      (let [orientable (u/fmap (partial orient-against-all-known oriented) to-orient)
+            fixed      (into {} (filter (comp some? val) orientable))]
+        (recur (merge oriented fixed)
+               (u/without-keys to-orient (keys fixed)))))))
+
+(defn all-beacons
+  [sensors]
+  (into #{} (mapcat :beacons (vals (orient-all-sensors sensors)))))
+
+(defn day19-part1-soln
+  []
+  (count (all-beacons day19-input)))
+
+(defn max-sensor-distance
+  [sensors]
+  (let [offsets (filter some? (map :offset (vals (orient-all-sensors sensors))))
+        indices (range (count offsets))]
+    (->> (mapv #(u/rotate % offsets) indices)
+         (map #(map (partial dist (first %)) (rest %)))
+         flatten
+         (apply max))))
+
+(defn day19-part2-soln
+  []
+  (max-sensor-distance day19-input))
--- test/advent2021/day19_test.clj
+++ test/advent2021/day19_test.clj
@@ -1,11 +1,27 @@
 (ns advent2021.day19-test
   (:require [clojure.test :refer [deftest testing is]]
+            [advent-utils.core :as u]
             [advent2021.day19 :as t]))
 
-;; (deftest day19-part1-soln
-;;   (testing "Reproduces the answer for day19, part1"
-;;     (is (= 394647 (t/day19-part1-soln)))))
+(def day19-sample1 (u/fmap t/add-relative-vectors (t/parse (u/puzzle-input "day19-sample1.txt"))))
 
-;; (deftest day19-part2-soln
-;;   (testing "Reproduces the answer for day19, part2"
-;;     (is (= 2380061249 (t/day19-part2-soln)))))
+(deftest orient-sensor-to-known
+  (testing "Can compute the offset for sensor 1 to sensor 0 in the sample data"
+    (is (= [68 -1246 -43]
+           (:offset (t/orient-sensor-to-known (get day19-sample1 0) (get day19-sample1 1)))))))
+
+(deftest count-all-beacons
+  (testing "Counts all the beaconsin the sample data"
+    (is (= 79 (count (t/all-beacons day19-sample1))))))
+
+(deftest max-sensor-distance
+  (testing "Finds the maximum (Manhattan) distance between any two sensors"
+    (is (= 3621 (t/max-sensor-distance day19-sample1)))))
+
+(deftest day19-part1-soln
+  (testing "Reproduces the answer for day19, part1"
+    (is (= 449 (t/day19-part1-soln)))))
+
+(deftest day19-part2-soln
+  (testing "Reproduces the answer for day19, part2"
+    (is (= 13128 (t/day19-part2-soln)))))
