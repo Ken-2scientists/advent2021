83eadc1de25471dbd85d8e286bed4052d45c02c3
Ken Smith <ken.smith@2scientists.net>
2021-12-28 17:12:02 -0700

    Day 25 solution

--- src/advent2021/day25.clj
+++ src/advent2021/day25.clj
@@ -1,4 +1,52 @@
 (ns advent2021.day25
-  (:require [advent-utils.core :as u]))
+  (:require [advent-utils.ascii :as ascii]
+            [advent-utils.core :as u]))
 
-;; (def day25-input (u/puzzle-input "day25-input.txt"))
+(def charmap {\v :down \> :right \. :open})
+
+(def day25-input
+  (ascii/ascii->map charmap (u/puzzle-input "day25-input.txt")))
+
+(defn available?
+  [grid pos]
+  (= :open (get grid pos)))
+
+(defn right-from
+  [{:keys [width]} [x y]]
+  (if (= (inc x) width)
+    [0 y]
+    [(inc x) y]))
+
+(defn down-from
+  [{:keys [height]} [x y]]
+  (if (= (inc y) height)
+    [x 0]
+    [x (inc y)]))
+
+(defn move-dir
+  [label direction {:keys [grid] :as state}]
+  (let [locs        (map first (filter #(= label (val %)) grid))
+        moveto-locs (map (partial direction state) locs)
+        moves      (->> (zipmap locs moveto-locs)
+                        (filter #(available? grid (val %)))
+                        (into {}))]
+    (update state :grid merge
+            (zipmap (keys moves) (repeat :open))
+            (zipmap (vals moves) (repeat label)))))
+
+(defn step
+  [state]
+  (->> state
+       (move-dir :right right-from)
+       (move-dir :down down-from)))
+
+(defn evolve-until-stop
+  [input]
+  (loop [last-state input curr-state (step input) steps 1]
+    (if (= last-state curr-state)
+      [steps curr-state]
+      (recur curr-state (step curr-state) (inc steps)))))
+
+(defn day25-part1-soln
+  []
+  (first (evolve-until-stop day25-input)))
--- test/advent2021/day25_test.clj
+++ test/advent2021/day25_test.clj
@@ -1,11 +1,25 @@
 (ns advent2021.day25-test
   (:require [clojure.test :refer [deftest testing is]]
+            [advent-utils.ascii :as ascii]
             [advent2021.day25 :as t]))
 
-;; (deftest day25-part1-soln
-;;   (testing "Reproduces the answer for day25, part1"
-;;     (is (= 394647 (t/day25-part1-soln)))))
+(def day25-sample
+  (ascii/ascii->map t/charmap
+                    ["v...>>.vv>"
+                     ".vv>>.vv.."
+                     ">>.>v>...v"
+                     ">>v>>.>.v."
+                     "v>v.vv.v.."
+                     ">.>>..v..."
+                     ".vv..>.>v."
+                     "v.v..>>v.v"
+                     "....v..v.>"]))
 
-;; (deftest day25-part2-soln
-;;   (testing "Reproduces the answer for day25, part2"
-;;     (is (= 2380061249 (t/day25-part2-soln)))))
+(deftest steps-until-done-changing
+  (testing "Computes the number of steps until no more changes occur"
+    (is (= 58 (first (t/evolve-until-stop day25-sample))))))
+
+(deftest day25-part1-soln
+  (testing "Reproduces the answer for day25, part1"
+    (is (= 429 (t/day25-part1-soln)))))
+
